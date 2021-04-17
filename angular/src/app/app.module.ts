358e903e2606bc7c29b7e8c6b9923a3ab1f1c041
freddysilber <freddy.silber@gmail.com>
2021-04-17 17:45:57 -0600

    material buttons learning

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { MatTabsModule } from '@angular/material/tabs';
+import { MatButtonModule } from '@angular/material/button'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
@@ -26,6 +27,7 @@ const COMPONENTS = [
     BrowserModule,
     HttpClientModule,
     MatTabsModule,
+    MatButtonModule,
     AppRoutingModule,
     BrowserAnimationsModule,
   ],
