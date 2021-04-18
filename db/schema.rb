966ad4ed36688a8ef8659145bc0bc81c051265e1
freddysilber <freddy.silber@gmail.com>
2021-04-17 18:59:51 -0600

    adds fields to user

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,8 @@
 ActiveRecord::Schema.define(version: 2021_04_16_023417) do
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "first_name"
+    t.string "last_name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
