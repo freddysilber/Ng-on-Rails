57585e50223c9d4713005f81f784922f3d6c4915
freddysilber <freddy.silber@gmail.com>
2021-04-17 19:34:27 -0600

    indentation

--- angular/src/app/components/users/users.component.ts
+++ angular/src/app/components/users/users.component.ts
@@ -2,18 +2,18 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
-  selector: 'app-users',
-  templateUrl: './users.component.html',
-  styleUrls: ['./users.component.scss']
+	selector: 'app-users',
+	templateUrl: './users.component.html',
+	styleUrls: ['./users.component.scss']
 })
 export class UsersComponent implements OnInit {
 
-  constructor(
-      private readonly _route: ActivatedRoute
-  ) { }
+	constructor(
+		private readonly _route: ActivatedRoute
+	) { }
 
-  ngOnInit() {
-    console.log(this._route);
-  }
+	ngOnInit() {
+		console.log(this._route);
+	}
 
 }
