9162cd22cc30a3f383a0a7e86edcf1d91f9098cc
freddysilber <freddy.silber@gmail.com>
2021-05-04 22:06:55 -0600

    learning d3

--- angular/src/modules/d3/components/d3-index/d3.component.ts
+++ angular/src/modules/d3/components/d3-index/d3.component.ts
@@ -7,15 +7,19 @@ import * as d3 from 'd3';
 })
 export class D3IndexComponent implements OnInit, AfterViewInit {
 
-	ngOnInit() {
-		console.log('hello D3');
-	}
+	ngOnInit() { }
 
 	ngAfterViewInit() {
 		this.buildSampleChart();
 	}
 
 	private buildSampleChart() {
-		d3.select('d3Container').append('svg')
+		d3.selectAll('.d3Container')
+			.append('h1')
+			.text('hello d3')
+
+		// d3.selectAll('p')
+		// 	.attr('class', 'graf')
+		// 	.style('color', 'red');
 	}
 }
