2a6ab19c8a6d6d260fd080932816b41f5064f9c7
freddysilber <freddy.silber@gmail.com>
2021-05-04 19:03:17 -0600

    ????

--- angular/src/modules/d3/d3.module.ts
+++ angular/src/modules/d3/d3.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { D3IndexComponent } from './components';
+import { D3RoutingModule } from './d3-routing.module';
 
 const COMPONENTS = [
 	D3IndexComponent
@@ -9,12 +10,16 @@ const COMPONENTS = [
 
 @NgModule({
 	imports: [
-		CommonModule
+		CommonModule,
+		D3RoutingModule
 	],
 	exports: [],
 	declarations: [
 		COMPONENTS
 	],
-	providers: []
+	providers: [],
+	bootstrap: [
+		D3IndexComponent
+	]
 })
 export class D3Module { }
