a39874714e2d2a4fabb635379389d20bf028fde9
freddysilber <freddy.silber@gmail.com>
2021-05-12 19:25:18 -0600

    stuff

--- angular/src/modules/d3/components/d3-index/d3.component.ts
+++ angular/src/modules/d3/components/d3-index/d3.component.ts
@@ -1,25 +1,29 @@
-import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import * as d3 from 'd3';
 
+const DATA = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 @Component({
 	selector: 'app-d3-index',
 	templateUrl: 'd3.component.html'
 })
 export class D3IndexComponent implements OnInit, AfterViewInit {
 
-	ngOnInit() { }
+	private _d3Container;
+
+	ngOnInit() {
+		console.log(DATA);
+	}
 
 	ngAfterViewInit() {
 		this.buildSampleChart();
 	}
 
 	private buildSampleChart() {
-		d3.selectAll('.d3Container')
-			.append('h1')
-			.text('hello d3')
-
-		// d3.selectAll('p')
-		// 	.attr('class', 'graf')
-		// 	.style('color', 'red');
+		d3.select('.d3Container')
+			.selectAll('p')
+			.data(DATA)
+			.enter()
+			.append('p')
+			.text((d) => d)
 	}
 }
